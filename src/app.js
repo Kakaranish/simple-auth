188a4bb01c26a17546cd935479eb18d8705f19a5
Kakaranish <stasiekgruz@gmail.com>
2020-05-05 10:40:09 +0200

    Added mongoose

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => console.log("Connection with db has been established.")).catch(error => {
+    console.log("Establishing connection with db failed");
+    throw error;
+});
 
 app.get('/test', async (req, res) => {
     res.status(200).json({
